25f1b890e3e6505cd8078f0f21db8e340bf09432
jtv3000 <60490523+spyder3000@users.noreply.github.com>
2020-08-23 01:41:12 -0400

    ch 168 - Store user data in user area

--- src/components/HelpPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';   // using ES6 syntax for React;  
-
-const HelpExpensePage = () => (
-    <div>Help Expense component</div>
-);
-
-export default HelpExpensePage; 
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -5,10 +5,10 @@ import createHistory from 'history/createBrowserHistory';
 import ExpenseDashBoardPage from '../components/ExpenseDashBoardPage'; 
 import AddExpensePage from '../components/AddExpensePage'; 
 import EditExpensePage from '../components/EditExpensePage'; 
-import HelpExpensePage from '../components/HelpPage'; 
 import NotFoundPage from '../components/NotFoundPage'; 
 import LoginPage from '../components/LoginPage'; 
 import PrivateRoute from './PrivateRoute'; 
+import PublicRoute from './PublicRoute'; 
 
 export const history = createHistory(); 
 
@@ -17,11 +17,10 @@ const AppRouter = () => (   // this is JSX;  <Route /> includes 2 props -- path 
     <Router history={history}> 
         <div>   {/*  <div> needed if using more than one <Route />;  */}
             <Switch>     
-                <Route path="/" component={LoginPage} exact={true}/>   {/* need exact stmt so it doesnt match /create & others */}
+                <PublicRoute path="/" component={LoginPage} exact={true}/>   {/* need exact stmt so it doesnt match /create & others */}
                 <PrivateRoute path="/dashboard" component={ExpenseDashBoardPage}/>   
                 <PrivateRoute path="/create" component={AddExpensePage} />
                 <PrivateRoute path="/edit/:id" component={EditExpensePage} />
-                <Route path="/help" component={HelpExpensePage} />
                 <Route component={NotFoundPage} />  
             </Switch>
         </div>
